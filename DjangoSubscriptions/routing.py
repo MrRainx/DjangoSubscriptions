28398a2ee11869b5a87388f475945cd7dc1a0b10
MrRainx <diegocondo10@yahoo.es>
2019-10-25 00:07:50 -0500

    solo tengo que tranformar a observable y listo :v

--- DjangoSubscriptions/routing.py
+++ DjangoSubscriptions/routing.py
@@ -1,4 +1,5 @@
 from channels.routing import route_class
+
 from graphql_ws.django_channels import GraphQLSubscriptionConsumer
 
 channel_routing = [
--- DjangoSubscriptions/schema.py
+++ DjangoSubscriptions/schema.py
@@ -1,5 +1,12 @@
 import graphene
+from django.contrib.auth import get_user_model
+from graphene import relay
 from rx import Observable
+
+import graphene
+
+# from DjangoSubscriptions.graphql.schema import Subscriptions
+from DjangoSubscriptions.graphql.schema import UserType
 
 
 class Query(graphene.ObjectType):
@@ -9,19 +16,30 @@ class Query(graphene.ObjectType):
         return 'world'
 
 
+'''
 class Subscription(graphene.ObjectType):
     count_seconds = graphene.Int(up_to=graphene.Int(required=True))
+    user_subscription = UserSubscription.Field()
 
     def resolve_count_seconds(root, info, up_to=5):
         return 1
-        '''
         return Observable.interval(1000) \
             .map(lambda i: "{0}".format(i)) \
             .take_while(lambda i: int(i) <= up_to)
-        '''
+'''
+
+
+class RootSubscription(graphene.ObjectType):
+    test = graphene.Field(UserType)
+
+    def resolve_user(self, info):
+        return get_user_model().objects.all()
+
+    class Meta:
+        description = 'The project root subscription definition'
 
 
 schema = graphene.Schema(
     query=Query,
-    subscription=Subscription
+    subscription=RootSubscription
 )
